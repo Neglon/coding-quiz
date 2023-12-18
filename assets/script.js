408e9a6ca83c0b9662d4a2b0e403122397b40c8b
Neglon <thomasfneylon@gmail.com>
2023-12-18 15:26:38 -0500

    timer feature basic up and running
    timer counts down and appears on screen and stops at 0

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
 //variables targetting id's in the DOM
 var startButton = document.getElementById('startQuiz');
 var introEl = document.getElementById('intro');
+var timerEL = document.getElementById('timer');
 
+//variables
+var time = 10;
+//setting timeInterval variable to gobal scobe so that it can be accessed in multiple functions
+var timeInterval;
 
 //function to start the quiz
 function startQuiz() {
@@ -10,12 +15,26 @@ function startQuiz() {
     // to test that the button works on click
     // console.log("start quiz")
 
-    
+    timeInterval = setInterval(timer, 1000);
+
+
 
 
 }
 
-
+function timer() {
+    if (time <= 0){
+        clearInterval(timeInterval);
+        //seems below code isnt necessary as i messed around with commenting them in and out  will leave for now
+        // time = 0;
+        // timerEL.textContent = time;
+        return;
+    }
+    
+    time--;
+    timerEL.textContent = time;
+    
+}
 
 
 
